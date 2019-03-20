772dc841b19ae743a9aced2ef37261589b4e4bf1
VitalMos <moiseenokv@gmail.com>
2019-03-20 17:22:18 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    beginLength=str.length;
+  for(k=0; k<=bracketsConfig.length-1; k++){   
+      var suchen =String(bracketsConfig[k][0]+bracketsConfig[k][1]);
+      str = str.replace(suchen,'');
+      if(beginLength > str.length && str.length!==0){
+          check(str, bracketsConfig);
+      }else if(beginLength==str.length && str.length!==0){
+          value=false;
+      }else{
+          value=true;
+      }
+  }
+  return value
 }
